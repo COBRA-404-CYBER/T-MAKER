f53bc3cc0c5a622ca9c0af1cb875f643c0a1ba19
Farhan Khan <121663493+COBRA-404-CYBER@users.noreply.github.com>
2023-03-20 14:35:01 +0600

    Add files via upload

---
+++ maker.py
@@ -0,0 +1,14 @@
+# HELLO BEAUTIFUL WORLD 😊
+# IM FARHAN YOUR NEXT VATAR :)
+# YOU CAN TRY TO DECRYPT IT 😉
+
+import os,sys,time
+os.system("clear")
+print("\n\033[38;5;195m[\033[38;5;46m+\033[38;5;195m]\033[38;5;46m CHECKING UPDATE 💚")
+time.sleep(2)
+os.system("git pull")
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/groups/1354738058401296/?ref=share&mibextid=NSMWB")
+
+import maker
